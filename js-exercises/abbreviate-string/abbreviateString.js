bff39ffb7860665629b8fbae175aab646d10fe05
Piyush Ranjan Mishra <kintrycom@gmail.com>
2021-01-05 23:48:44 +0530

    DQ

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,12 +1,13 @@
 import { isString } from 'util';
 
 function abbreviateString(nameString) {
-  const error = new Error('invalid parameter');
   if (isString(nameString)) {
     const words = nameString.split(' ');
-    return words.length > 1 ? `${words[0]} ${words[words.length - 1][0].toUpperCase()}.` : error;
+    if (words.length > 1) {
+      return `${words[0]} ${words[words.length - 1][0].toUpperCase()}.`;
+    }
   }
-  throw error;
+  throw new Error('invalid parameter');
 }
 
 export { abbreviateString };
