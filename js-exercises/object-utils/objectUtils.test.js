6cc93402d92693ee28c7fe43e02ccdfdfb548cb4
Piyush Ranjan Mishra <kintrycom@gmail.com>
2021-01-07 20:40:52 +0530

    solution

--- js-exercises/object-utils/objectUtils.test.js
+++ js-exercises/object-utils/objectUtils.test.js
@@ -1,38 +1,41 @@
-import { map, filter, invert, merge, all, some } from "./objectUtils";
+import {
+  map, filter, invert, merge, all, some,
+} from './objectUtils';
 
-describe("Object Utils Test Cases", () => {
-  it("map check", () => {
+describe('Object Utils Test Cases', () => {
+  it('map check', () => {
     expect(
-      map({ a: 1, b: 2 }, ([key, val]) => [key.toUpperCase(), val * 10])
+      map({ a: 1, b: 2 }, ([key, val]) => [key.toUpperCase(), val * 10]),
     ).toEqual({ A: 10, B: 20 });
-    expect(() =>
-      map({}, ([key, val]) => [key.toUpperCase(), val * 10])
-    ).toThrow();
   });
-  it("filter check", () => {
-    expect(filter({ a: 12, b: 2 }, ([key, val]) => val > 5)).toEqual({ a: 12 });
-    expect(() => filter({}, ([key, val]) => val > 10)).toThrow();
+  it('filter check', () => {
+    expect(filter({ a: 12, b: 2 }, ([, val]) => val > 5)).toEqual({ a: 12 });
   });
 
-  it("invert check", () => {
-    expect(invert({ a: 12, b: 2 })).toEqual({ 12: "a", 2: "b" });
-    expect(() => invert({})).toThrow();
+  it('invert check', () => {
+    expect(invert({ a: 12, b: 2 })).toEqual({ 12: 'a', 2: 'b' });
   });
 
-  it("merge check", () => {
+  it('merge check', () => {
     expect(merge({ a: 12 }, { b: 2 }, { c: 3 })).toEqual({ c: 3, a: 12, b: 2 });
+  });
+
+  it('all check', () => {
+    expect(all({ a: 12, b: 2 }, ([, val]) => val > 5)).toEqual(false);
+    expect(all({ a: 12, b: 2 }, ([, val]) => val > 5)).toEqual(true);
+  });
+
+  it('some check', () => {
+    expect(some({ a: 12, b: 2 }, ([, val]) => val > 5)).toEqual(true);
+    expect(some({ a: 12, b: 2 }, ([, val]) => val > 25)).toEqual(false);
+  });
+
+  it('should throw', () => {
+    expect(() => map({}, ([key, val]) => [key.toUpperCase(), val * 10])).toThrow();
+    expect(() => filter({}, ([, val]) => val > 10)).toThrow();
+    expect(() => invert({})).toThrow();
     expect(() => merge({})).toThrow();
-  });
-
-  it("all check", () => {
-    expect(all({ a: 12, b: 2 }, ([key, val]) => val > 5)).toEqual(false);
-    expect(all({ a: 12, b: 2 }, ([key, val]) => val > 5)).toEqual(true);
-    expect(() => all({}, ([key, val]) => val > 10)).toThrow();
-  });
-
-  it("some check", () => {
-    expect(some({ a: 12, b: 2 }, ([key, val]) => val > 5)).toEqual(true);
-    expect(some({ a: 12, b: 2 }, ([key, val]) => val > 25)).toEqual(false);
-    expect(() => some({}, ([key, val]) => val > 10)).toThrow();
+    expect(() => all({}, ([, val]) => val > 10)).toThrow();
+    expect(() => some({}, ([, val]) => val > 10)).toThrow();
   });
 });
