841c3810dbd1fa00b8b40306bbf7062661b53837
Piyush Ranjan Mishra <kintrycom@gmail.com>
2021-01-12 23:43:23 +0530

    solutions-sleep-promise

---
+++ js-exercises/sleep-promise/sleepPromise.js
@@ -0,0 +1,18 @@
+const sleep = ms => {
+  if (ms < 0) {
+    throw new RangeError(`${ms}ms!!! You are not in time machine, you can't go back :)`);
+  }
+
+  const promise = new Promise((resolve) => setTimeout(resolve, ms));
+
+  // new function object
+  const customPromise = val => promise.then(() => val);
+
+  // define its own then and catch method
+  customPromise.then = (...args) => promise.then(...args);
+  customPromise.catch = Promise.resolve().catch;
+
+  return customPromise;
+};
+
+export { sleep };
